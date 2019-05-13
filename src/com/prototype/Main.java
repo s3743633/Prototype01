c6c159091831c1577db4fd99c919a9f34309a61e
Grant Cavanagh <s3743633@student.rmit.edu.au>
2019-05-13 22:00:51 +1000

    basic map navigation, validation and unique check

--- src/com/prototype/Main.java
+++ src/com/prototype/Main.java
@@ -4,19 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
-Scanner input = new Scanner(System.in);
-Player wolf = new Player("wolf", 50, 1, 100, true);
-Player player1 = new Player("bob", 100, 10,30, true);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Player wolf = new Player("wolf", 50, 1, 100, true, 0);
+        Player player1 = new Player("bob", 100, 10, 30, true, 0);
+        Maps mapa1 = new Maps(false, "beach", 0);
 
-System.out.println("fight test");
+        System.out.println("you wake up and find yourself stranded on the coast, you see that there is a path that leads north");
+        mapa1.mapA4();
 
-  //player1.attack(player1, wolf);
-        //player1.attack(player1, wolf);
-       // player1.attack(player1, wolf);
-  //wolf.attack(player1, wolf);
-
- player1.fight(player1, wolf);
 
     }
 
