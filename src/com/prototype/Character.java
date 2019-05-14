b32725735901157974bef11d7f4cfc0314da4f83
Chris <s3742928@student.rmit.edu.au>
2019-05-14 13:40:47 +1000

    loop menu

--- src/com/prototype/Character.java
+++ src/com/prototype/Character.java
@@ -76,27 +76,27 @@ public abstract class Character {
 
         public void attack(Player a, Player b) {
             Random rand = new Random();
-            int damage = Math.max(0,rand.nextInt(a.getDamage()) - b.getDefence());
-            int monsDamage = Math.max(0,rand.nextInt(b.getDamage()) - a.getDefence());
-            int health = Math.max(0,b.getHealth() - damage);
+            int damage = Math.max(0, rand.nextInt(a.getDamage()) - b.getDefence());
+            int monsDamage = Math.max(0, rand.nextInt(b.getDamage()) - a.getDefence());
+            int health = Math.max(0, b.getHealth() - damage);
             b.setHealth(health);
             System.out.println("you hit a " + damage);
             System.out.println(b.getName() + " hp is now:" + health);
-            if(!b.isAlive()){
+            if (!b.isAlive()) {
                 System.out.println("You killed: " + b.getName());
-            }
-            else {
-                int yourHP = Math.max(0,a.getHealth() - monsDamage);
+            } else {
+                int yourHP = Math.max(0, a.getHealth() - monsDamage);
                 a.setHealth(yourHP);
                 System.out.println(b.getName() + " did " + monsDamage);
-                System.out.println("your health is now: " + yourHP);}
+                System.out.println("your health is now: " + yourHP);
+            }
         }
 
 
         public void fight(Player a, Player b) {
             Scanner input = new Scanner(System.in);
 
-            do{
+            do {
                 System.out.println("what do you want to do?");
                 System.out.println("attack or runaway?");
                 String choice = input.nextLine();
@@ -105,54 +105,75 @@ public abstract class Character {
                     attack(a, b);
                 }
             } while (a.isAlive() && b.isAlive());
-                if(!a.isAlive()){
-                    System.out.println("RIP Game Over!");
-                }
+            if (!a.isAlive()) {
+                System.out.println("RIP Game Over!");
+            }
 
         }
 
-        public void menu(Player a){
-            Scanner input = new Scanner(System.in);
-            System.out.println("Equip item");
-            System.out.println("Use Potion");
-            System.out.println("Check Status");
-            String menu = input.nextLine();
+        public void menu(Player a) {
+            int loop = 0;
+            while (loop == 0) {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Equip item");
+                System.out.println("Use Potion");
+                System.out.println("Check Status");
+                System.out.println("Exit");
+                String menu = input.nextLine();
+                loop = 1;
 
-                if (menu.equalsIgnoreCase("equip item")){
-                    System.out.println("what do you want to equip?");
+                while (loop == 1) {
+                    if (menu.equalsIgnoreCase("equip item")) {
+                        System.out.println("what do you want to equip?");
+                        loop = 0;
 
-                }else if(menu.equalsIgnoreCase("use potion")){
-                    System.out.println("You have "+ numOfPotions + "potion(s) left.");
-                    System.out.println("Would you like to use a potion?");
-                    System.out.println("\tYes");
-                    System.out.println("\tNo");
-                    String choice = input.nextLine();
-                    boolean potmenu = false
-                            while (!potmenu)
-                        if (choice.equalsIgnoreCase("Yes")) {
-                            if (numOfPotions >= 1){
+                    } else if (menu.equalsIgnoreCase("use potion")) {
+                        System.out.println("You have " + numOfPotions + "potion(s) left.");
+                        System.out.println("Would you like to use a potion?");
+                        System.out.println("\tYes");
+                        System.out.println("\tNo");
+                        String choice = input.nextLine();
+                        loop = 2;
+
+                        while (loop == 2) {
+                            if (choice.equalsIgnoreCase("Yes")) {
+                                if (numOfPotions >= 1) {
                                     int yourHP = Math.min(100, a.getHealth() + potHealAmount);
                                     a.setHealth(yourHP);
                                     numOfPotions--;
                                     System.out.println("Your current HP is now: " + a.getHealth());
+                                    loop = 1;
 
-                            } else if (numOfPotions == 0){
-                                        System.out.println("Sorry, you are out of potions!");
+                                } else if (numOfPotions == 0) {
+                                    System.out.println("Sorry, you are out of potions!");
+                                    loop = 0;
 
+                                }
+                            } else if (choice.equalsIgnoreCase("No")) {
+                                loop = 0;
+
+                            } else {
+                                System.out.println("Invalid input");
+                                loop = 1;
                             }
-                        } else if(choice.equalsIgnoreCase("No")){
-
-                        } else { System.out.println("Invalid input");
                         }
 
+                    } else if (menu.equalsIgnoreCase("check status")) {
+                        System.out.println("Your current stats are: ");
+                        System.out.println("Name = " + a.getName());
+                        System.out.println("Health = " + a.getHealth());
+                        System.out.println("Damage = " + a.getDamage());
+                        System.out.println("Defence = " + a.getDefence());
+                        loop = 0;
 
-                }else if(menu.equalsIgnoreCase("check status")){
-                    System.out.println("Your current stats are: ");
-                    System.out.println("Name = " + a.getName());
-                    System.out.println("Health = " + a.getHealth());
-                    System.out.println("Damage = " + a.getDamage());
-                    System.out.println("Defence = " + a.getDefence());
-                }else
-                    System.out.println("Invalid input, try again");
+                    } else if (menu.equalsIgnoreCase("Exit")){
+                      break;
+                    }
+                    else {
+                        System.out.println("Invalid input, try again");
+                        loop = 0 ;
+                    }
+                }
             }
         }
+    }
